94528feed035aff092270a0f09a643ae3274a4e6
Apprentice <apprentice@dbc24.local>
2013-03-15 14:16:30 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicTacRails::Application.config.secret_token = 'b9f5dd056308dfeb281339c501e79f2906503fcff8bbaa1afc200f892503e23eae2b4d66b78b6c13ca6ac47d469491b88b889d13cf5f2b22f970210989cdd59b'
