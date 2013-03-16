c8e1e912f26047dcb8a4350cb722fd7fa789b68e
Apprentice <apprentice@dbc24.local>
2013-03-16 15:08:26 -0700

    Add show page with board

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,9 @@
 module GamesHelper
+  def icon(board_cell)
+    if board_cell ==  "X"
+      image_tag "/assets/X.png", class: "board-icon"
+    elsif board_cell ==  "O"
+      image_tag "/assets/O.png", class: "board-icon"
+    end
+  end
 end
